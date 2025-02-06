595ebda5b12266f1ac57fb6f08dca27033f52c76
Alexis Reubrecht <lxsrbrcht@gmail.com>
2025-02-06 13:58:25 +0100

    multiples fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_projects
+
+
+  def set_projects
+    @projects = Project.all
+  end
 end
